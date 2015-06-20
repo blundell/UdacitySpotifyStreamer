8d93874f5d1568238977def09e797cc6fc765682
Blundell <blundell.app.dev@gmail.com>
2015-06-20 16:02:37 +0100

    Adds initial mock screens

---
+++ app/src/main/java/com/blundell/udacityspotifystreamer/SearchArtistActivity.java
@@ -0,0 +1,13 @@
+package com.blundell.udacityspotifystreamer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchArtistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_artist);
+    }
+}
---
+++ app/src/main/java/com/blundell/udacityspotifystreamer/ViewTracksActivity.java
@@ -0,0 +1,13 @@
+package com.blundell.udacityspotifystreamer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewTracksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_tracks);
+    }
+}
